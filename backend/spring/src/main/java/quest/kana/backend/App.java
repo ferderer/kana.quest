8d28d9f25ccc47908fe1919b25510d49db63651f
Vadim Ferderer <vadim@ferderer.de>
2025-07-09 08:08:45 +0200

    create backend project

---
+++ backend/spring/src/main/java/quest/kana/backend/App.java
@@ -0,0 +1,12 @@
+package quest.kana.backend.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String... args) {
+        SpringApplication.run(App.class, args);
+    }
+}
